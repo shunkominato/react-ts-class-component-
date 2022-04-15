43866cef6575bbc665822a2f31c9051da031afa1
shunkominato <shun19871001@gmail.com>
2022-04-16 00:11:47 +0900

    a

--- src/pages/BBB/index.tsx
+++ src/pages/BBB/index.tsx
@@ -1,10 +1,41 @@
+/* eslint-disable */
 import React from 'react';
+import { Formik } from 'formik';
 
-class BBB extends React.Component {
+type State = {
+  todo: string;
+};
+
+class BBB extends React.Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      todo: '',
+    };
+    this.onChangeTodo = this.onChangeTodo.bind(this);
+  }
+
+  onChangeTodo(e: React.ChangeEvent<HTMLInputElement>) {
+    console.log(e)
+    this.setState({
+      todo: e.target.value,
+    })
+  }
   render() {
     return (
       <>
         todoリスト
+        <Formik
+          initialValues={{ todo: '' }}
+          onSubmit={(values) => console.log(values)}
+          render={(props) => (
+            <input
+              name='todo'
+              value={this.state.todo}
+              onChange={(e) => this.onChangeTodo(e)}
+            />
+          )}
+        ></Formik>
       </>
     );
   }
